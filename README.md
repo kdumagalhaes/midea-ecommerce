1ad779f78c6fd07be614ea3094e15eebb9d16f88
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-08-24 13:43:24 -0300

    finished layout && added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Copy of the Midea's store homepage
+
+In this challenge I used HTML5, LESS and JS Vanilla.
+
+![](assets/screen-capture.png)
+
+## Obs:
+- [x] First time using LESS;
+- [x] Slider using JS Vanilla;
+- [x] Added more breakpoints;
+- [x] Good score on Google Lighthouse;
+
+
+**You can see the live demo [clicking here](https://midea-ecommerce.vercel.app/).**
--- js/main.js
+++ js/main.js
@@ -1,104 +1,108 @@
-
 //slider configurations
 let time = 2500;
 let currentImageIndex = 0;
 let images = document.querySelectorAll('.c-top-slider__img');
 let max = images.length;
 
-let imagesResponsive = document.querySelectorAll('.c-top-slider__img--responsive')
+let imagesResponsive = document.querySelectorAll(
+  '.c-top-slider__img--responsive'
+);
 
 const topSlider = document.querySelector('c-top-slider');
 const previous = document.querySelector('.previous');
 const next = document.querySelector('.next');
 
 //responsive menu configurations
-const overlayDiv = document.querySelector('.overlay')
-const hamburgerNav = document.querySelector('.fa-bars')
-const navResponsive = document.querySelector('.nav--responsive')
+const overlayDiv = document.querySelector('.overlay');
+const hamburgerNav = document.querySelector('.fa-bars');
+const navResponsive = document.querySelector('.nav--responsive');
 
 //scroll up configurations
-const scrollUpBtn = document.querySelector('.scroll-up')
+const scrollUpBtn = document.querySelector('.scroll-up');
 
 function startJs() {
-
+  
   //scroll up button
 
   //btn show control
   const scrolling = () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollUpBtn.style.display = "block";
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollUpBtn.style.display = 'block';
     } else {
-      scrollUpBtn.style.display = "none";
+      scrollUpBtn.style.display = 'none';
     }
-  }
+  };
 
   window.onscroll = function () {
-    scrolling()
-  }
+    scrolling();
+  };
 
-  //
+  //scrolling to the top
   scrollUpBtn.addEventListener('click', () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  })
+  });
 
   //responsive menu
 
   //hamburger click opens menu
   hamburgerNav.addEventListener('click', () => {
-    overlayDiv.style.display = 'block'
-    navResponsive.style.left = '0'
-  })
+    overlayDiv.style.display = 'block';
+    navResponsive.style.left = '0';
+  });
 
   //overlay click closes responsive menu
   overlayDiv.addEventListener('click', () => {
-    overlayDiv.style.display = 'none'
-    navResponsive.style.left = '-5000px'
-  })
+    overlayDiv.style.display = 'none';
+    navResponsive.style.left = '-5000px';
+  });
 
   //top slider
 
   //slide imgs autoplay && clicking on 'next' arrow
   const slideImages = () => {
     images[currentImageIndex].classList.remove('selected-img');
-    imagesResponsive[currentImageIndex].classList.remove('selected-img--responsive');
+    imagesResponsive[currentImageIndex].classList.remove(
+      'selected-img--responsive'
+    );
 
-  
     currentImageIndex++;
-  
+
     if (currentImageIndex >= max) {
       currentImageIndex = 0;
     }
-  
-    images[currentImageIndex].classList.add('selected-img');
-    imagesResponsive[currentImageIndex].classList.add('selected-img--responsive');
 
+    images[currentImageIndex].classList.add('selected-img');
+    imagesResponsive[currentImageIndex].classList.add(
+      'selected-img--responsive'
+    );
   };
-  
+
   //slide imgs clicking on 'previous' arrow
   const previousSlide = () => {
     images[currentImageIndex].classList.remove('selected-img');
-  
+
     currentImageIndex--;
-  
+
     if (currentImageIndex === -1) {
       currentImageIndex = 5;
     }
-  
+
     images[currentImageIndex].classList.add('selected-img');
   };
-  
+
   //slide imgs function
   const startSlider = () => {
     setInterval(slideImages, time);
   };
 
-  startSlider()
-  
+  startSlider();
+
   next.addEventListener('click', slideImages);
   previous.addEventListener('click', previousSlide);
 }
-
-
 
 window.addEventListener('load', startJs);
